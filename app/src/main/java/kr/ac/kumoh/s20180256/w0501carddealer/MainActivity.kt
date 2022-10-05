d128e3c9242e2a30b657a9416bef5772f90e4d67
kiminchan <kiminchan12341@gmail.com>
2022-10-05 14:45:51 +0900

    🎉 add giti

--- app/src/main/java/kr/ac/kumoh/s20180256/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180256/w0501carddealer/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.ac.kumoh.s20180256.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20180256.w0501carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
